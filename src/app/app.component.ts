2a1b7e8898d65fc0afaef8a967419b2f881ed105
Amit Rai <arai@mobiquityinc.com>
2020-01-29 16:01:40 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ToDo } from './todo.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'searchBar';
+  searchTxt: string;
+  completeData: ToDo[];
+  searchArray: ToDo[];
+  frequentlySearchedTxt: [];
+
+  constructor(public http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/todos').subscribe((data: ToDo[]) => {
+      this.completeData = data;
+    });
+  }
+
+  fillTxt(txt) {
+    this.searchArray = this.completeData.filter((e) => {
+      return e.title.toLowerCase().includes(this.searchTxt.toLowerCase());
+    });
+    // }).sort((a, b) => {
+    //   return a;
+    // });
+  }
+
+  selectedSearchedTxt(item) {
+    this.searchArray = [item];
+  }
 }
